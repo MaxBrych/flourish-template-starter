a6ca44c57d6dc39cd76be9523a16209842263dab
MaxBrych <max.brych03@gmail.com>
2023-01-04 23:31:31 +0100

    initial commit

---
+++ template.js
@@ -0,0 +1,2 @@
+var template=function(e){"use strict";var t={h1_content:"Hello Nordkurier!",h1_color:"#28515F"};function o(){document.querySelector("h1").innerHTML=t.h1_content,document.querySelector("h1").style.color=t.h1_color}return e.data={},e.draw=function(){var e=document.createElement("h1");document.body.appendChild(e),o()},e.state=t,e.update=o,e}({});
+//# sourceMappingURL=template.js.map
